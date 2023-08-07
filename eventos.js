e84cca0b13791c38fbd84b40e311e2c694ff47a5
mateo1627 <mateogoni1627@outlook.com>
2023-08-07 20:37:05 -0300

    new branch created

--- eventos.js
+++ eventos.js
@@ -1,7 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
     let divElement = document.getElementById("container");
-
+    let boton = document.getElementById("ButtonFixed");
+    
     divElement.addEventListener("click", function() {
         alert("Hola! Soy el div");
+   
+    });
+    
+    boton.addEventListener("click", function(event) {
+       
+        event.stopPropagation();
     });
 });
+
