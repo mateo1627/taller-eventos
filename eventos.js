56940d02662ed9a40dacb8de6ca66e0ae3d724c8
mateo1627 <mateogoni1627@outlook.com>
2023-08-07 19:41:21 -0300

    styles and events added

---
+++ eventos.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let divElement = document.getElementById("container");
+
+    divElement.addEventListener("click", function() {
+        alert("Hola! Soy el div");
+    });
+});
